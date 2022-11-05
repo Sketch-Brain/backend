f63bf328b508d4bd7e5e1a56b7711979c5ca6f22
TrulyNotMalware <notleebutyee@gmail.com>
2022-11-05 22:16:57 +0900

    Bugfix CORS Filter

--- src/main/java/com/sketch/brain/backend/configurations/CorsFilter.java
+++ src/main/java/com/sketch/brain/backend/configurations/CorsFilter.java
@@ -4,15 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsFilter implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
-                .exposedHeaders("*")
-                .maxAge(3000);
-    }
+//@Configuration
+//public class CorsFilter implements WebMvcConfigurer {
+public class CorsFilter{
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+//                .exposedHeaders("*")
+//                .maxAge(3000);
+//    }
 }
